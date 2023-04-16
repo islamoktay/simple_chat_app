5d9965e378ddd006a9616d6cb2949b5ed6a8403d
Ahmet Islam Oktay <43931758+islamoktay@users.noreply.github.com>
2023-04-16 05:13:51 +0300

    init

---
+++ lib/core/base/simple_chat_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SimpleChatApp extends StatelessWidget {
+  const SimpleChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:simple_chat_app/core/base/simple_chat_app.dart';
+
+void main() {
+  runApp(const SimpleChatApp());
+}
+
